7d67f94e7c033751924fb6a741adb655999809c0
Daniil-Solo <daniil.solo1723@gmail.com>
2024-04-05 13:19:14 +0300

    correct input ui component

--- src/ui/input/input.tsx
+++ src/ui/input/input.tsx
@@ -1,17 +1,19 @@
 import "./input.css";
-import React, {ChangeEvent} from "react";
+import React, {ChangeEvent, KeyboardEvent} from "react";
 
 interface InputProps{
+    style?: React.CSSProperties;
     placeholder: string,
     type?: string,
     value: string | number,
     onChange: (e: ChangeEvent<HTMLInputElement>) => void,
+    onKeyDown?: (e: KeyboardEvent<HTMLInputElement>) => void,
 }
 
 const Input: React.FC<InputProps> = (props) => {
-    const {placeholder, type="text", value, onChange} = props;
+    const {placeholder, type="text", value, style = {}, onChange, onKeyDown = () => null} = props;
     return (
-        <input className="input" type={type} placeholder={placeholder} value={value} onChange={onChange}/>
+        <input className="input" style={style} type={type} placeholder={placeholder} value={value} onChange={onChange} onKeyDown={onKeyDown}/>
     )
 }
 export default Input;
