11fa7c61c32b766055e881eaf719d5bb40b90f71
Daniil-Solo <daniil.solo1723@gmail.com>
2024-05-13 20:06:40 +0300

    add skeleton and correct style

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./main.css"
 import "./reset.css"
 import {ToastContainer, Bounce} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-loading-skeleton/dist/skeleton.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
