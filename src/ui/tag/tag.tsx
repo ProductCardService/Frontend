e364523ca96ccd4692f86168048cf6db3f57738f
Daniil-Solo <daniil.solo1723@gmail.com>
2024-05-13 20:07:27 +0300

    add close sign for tag

--- src/ui/tag/tag.tsx
+++ src/ui/tag/tag.tsx
@@ -11,7 +11,9 @@ const Tag: React.FC<TagProps> = (props) => {
     const {title, onClick = () => null, removable = false} = props;
     return (
         <div className={removable? "tag tag_removable": "tag"} onClick={onClick} title={removable? "Нажмите для удаления": undefined}>
-            <span className="tag__circle"></span>
+            <span className="tag__circle">
+                <span>&times;</span>
+            </span>
             <span className="tag__text">
                 {title}
             </span>
